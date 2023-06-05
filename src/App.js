0aaf1cdcc3474cf501291c68eb30e7d656ed3ce1
Deiby-Zair <deybyzm@unicauca.edu.co>
2023-06-04 19:01:28 -0500

    Manejo de categorias y subcategorias

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css';
+import Categoria from './pages/Categoria';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Page404 from './pages/Page404';
@@ -18,6 +19,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/sobre' element={<Sobre />} />
           <Route path='/posts/:id' element={<Post />} />
+          <Route path='/categoria/:id/*' element={<Categoria />} />
           <Route path='*' element={<Page404 />} />
         </Routes>
       </Router>
